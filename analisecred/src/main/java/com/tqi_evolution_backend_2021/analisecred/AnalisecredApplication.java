6b1058ca52186c32a998da85fd789b023145578b
Adailton Simplicio <adailtonsimplicio.s@gmail.com>
2021-12-29 23:28:39 -0300

    Iniciando projeto spring initializr

---
+++ analisecred/src/main/java/com/tqi_evolution_backend_2021/analisecred/AnalisecredApplication.java
@@ -0,0 +1,13 @@
+package com.tqi_evolution_backend_2021.analisecred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalisecredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalisecredApplication.class, args);
+	}
+
+}
---
+++ analisecred/src/test/java/com/tqi_evolution_backend_2021/analisecred/AnalisecredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tqi_evolution_backend_2021.analisecred;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalisecredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

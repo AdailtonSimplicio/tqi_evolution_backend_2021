1334999c6e0a2e6e06ccb40c78a7900d59c9773e
Adailton Simplicio <adailtonsimplicio.s@gmail.com>
2022-01-01 23:57:34 -0300

    404Not Found

--- analisecred/src/main/java/com/tqi_evolution_backend_2021/analisecred/model/ClienteModel.java
+++ analisecred/src/main/java/com/tqi_evolution_backend_2021/analisecred/model/ClienteModel.java
@@ -1,96 +1,29 @@
 package com.tqi_evolution_backend_2021.analisecred.model;
 
-import com.sun.istack.NotNull;
-import org.springframework.lang.NonNull;
-
+import lombok.Data;
 import javax.persistence.*;
-import java.math.BigDecimal;
 
-@Entity(name = "cliente")
+@Data
+@Entity
+
 public class ClienteModel {
-
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
     @Id
-    public Integer id;
-    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    @Column(nullable = false)
     public String nome;
-    @NotNull
+    @Column(nullable = false)
     public String email;
-    @NotNull
+    @Column(nullable = false)
     public String cpf;
-    @NotNull
+    @Column(nullable = false)
     public String rg;
-    @NotNull
+    @Column(nullable = false)
     public String endereco;
-    @NotNull
+    @Column(nullable = false)
     public Float renda;
-    @NotNull
+    @Column(nullable = false)
     public String senha;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getRg() {
-        return rg;
-    }
-
-    public void setRg(String rg) {
-        this.rg = rg;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public Float getRenda() {
-        return renda;
-    }
-
-    public void setRenda(Float renda) {
-        this.renda = renda;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
 }
 
 

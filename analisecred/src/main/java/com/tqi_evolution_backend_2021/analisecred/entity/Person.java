5a95382622f15bff025d8e27853e8f30838a07d0
Adailton Simplicio <adailtonsimplicio.s@gmail.com>
2022-01-04 23:20:26 -0300

    Corrigindo conexão com o banco H2

--- analisecred/src/main/java/com/tqi_evolution_backend_2021/analisecred/entity/Person.java
+++ analisecred/src/main/java/com/tqi_evolution_backend_2021/analisecred/entity/Person.java
@@ -19,6 +19,7 @@ public class Person {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
     @Column(nullable = false)
     private String nomeCompleto;
     @Column(nullable = false, unique = true)
